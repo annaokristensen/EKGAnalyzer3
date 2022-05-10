d0e58df292c422f0a62fcbac49df24f0229322c9
Anna Kristensen <annaokristensen@gmail.com>
2022-05-10 16:49:36 +0200

    Algoritme opdateret

--- LogicLayer/Algoritme.cs
+++ LogicLayer/Algoritme.cs
@@ -18,13 +18,32 @@ namespace LogicLayer
         {
             List<double> EKGSample = ekg.EKGsamples;
 
+            //Antallet af R-takker tælles
             for (int i = 0; i < EKGSample.Count; i++)
             {
-                if(EKGSample[i] > PtakThreshold && belowThresholdPtak == true && EKGSample[i] < RtakThreshold)
+                if(EKGSample[i] > RtakThreshold && belowThresholdRtak == true)
+                {
+                    RtakCount++;
+                }
+                
+                if (EKGSample[i] < RtakThreshold)
+                {
+                    belowThresholdRtak = true;
+                }
+                else
+                {
+                    belowThresholdRtak = false;
+                }
+            }
+
+            //Antallet af alle takker tælles
+            for (int i = 0; i < EKGSample.Count; i++)
+            {
+                if (EKGSample[i] > PtakThreshold && belowThresholdPtak == true)
                 {
                     PtakCount++;
                 }
-                
+
                 if (EKGSample[i] < PtakThreshold)
                 {
                     belowThresholdPtak = true;
@@ -35,9 +54,10 @@ namespace LogicLayer
                 }
             }
 
-            double PtakMinute = (PtakCount / ekg.MeasurementPeriod) *60;
 
-            if (PtakMinute > 200)
+            PtakCount = PtakCount - RtakCount; //Da R-takker tælles med i P-tak count trækkes de fra her igen
+
+            if (PtakCount / RtakCount >=4) //Et forslag med forholdet 1 til 4
             {
                 return true;
             }
@@ -45,7 +65,38 @@ namespace LogicLayer
             {
                 return false;
             }
+
         }
 
     }
 }
+
+//List<double> EKGSample = ekg.EKGsamples;
+
+//for (int i = 0; i < EKGSample.Count; i++)
+//{
+//    if (EKGSample[i] > PtakThreshold && belowThresholdPtak == true && EKGSample[i] < RtakThreshold)
+//    {
+//        PtakCount++;
+//    }
+
+//    if (EKGSample[i] < PtakThreshold)
+//    {
+//        belowThresholdPtak = true;
+//    }
+//    else
+//    {
+//        belowThresholdPtak = false;
+//    }
+//}
+
+//double PtakMinute = (PtakCount / ekg.MeasurementPeriod) * 60;
+
+//if (PtakMinute > 200)
+//{
+//    return true;
+//}
+//else
+//{
+//    return false;
+//}
